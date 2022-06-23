d25cc8589661bd151def58b4deb7a265922e700c
ceccy-ope <muyidececilia@gmail.com>
2022-06-23 14:53:56 +0100

    name

---
+++ main.js
@@ -0,0 +1,3 @@
+const info={first_name:"Cecilia", last_name: "Muyide", Age:"40", Height:"1.6M", Country:"Nigerian"  }
+document.getElementById("p").innerHTML = "My name is"+ " " + info.first_name + " "  + info.last_name + "."+ " "+ "I am"+ " " + info.Age+ " " + "years old."
++ " "+ "My Height is"+ " "+ info.Height + "."+ " "+ "I am a "+ " "+ info.Country
